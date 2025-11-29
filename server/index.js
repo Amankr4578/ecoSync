e2e585720511aa0acb332d6e32de0b21b1ccce28
Sahil Yadav <im.sahil0111@gmail.com>
2025-11-29 22:00:01 +0530

    user authentication api

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -12,9 +14,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => {
     res.send('EcoSync API is running...');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
